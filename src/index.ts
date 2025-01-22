c6b98e6bfe2a1cda996de1a2ef08a32df30593bd
newcometlab <newcometlab@outlook.com>
2025-01-22 07:05:32 -0600

    add cors

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,11 @@ import { tasksRouter } from "./routes";
 const app: Express = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: "*", // Allows all origins
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Specify allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+}));
 app.use(express.json({ limit: "3mb" }));
 app.use(express.urlencoded({ limit: "3mb", extended: true }));
 
